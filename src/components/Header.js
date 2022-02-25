0b431486e3da1a00cc90c2b840fcfb8853fc8991
Oluwa-Seun <oluwaseunolubiyione@gmail.com>
2022-02-25 02:03:47 +0100

    add Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
-import Logo from "../asets/Logo-Nav.png";
+import Logo from "../assets/Logo-Nav.png";
 import { Link } from "react-router-dom";
 const Header = () => {
-    return ( 
-        <header className="flex md:flex-row justify-between item-center m-auto md:px-16 py-3 mb-4 font-poppins">
-            <Link to="/">
-                <img className="object-contain h-16" src={Logo}
+  return (
+    <header className="flex md:flex-row justify-between items-center m-auto md:px-16 py-3 mb-4 font-poppins">
+      <Link to="/">
+        <img className="object-contain h-16" src={Logo} alt="Logo" />
+      </Link>
 
-    )
-}
+      <div className="flex justify-between items-center text-white h-16">
+        <Link
+          to="/discover"
+          className="md:w-28 md:w-20 w-20 text-base hover:text-gray-200">
+          Discover
+        </Link>
+        <Link to="/join" className="md:w-16 w-10 text-base hover:text-gray-200">
+          Join
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
